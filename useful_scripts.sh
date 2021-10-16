fb53a799bf2258b97b1a72d9d4bbf3fc805426fc
Robert Hilton <robert.a.hilton.jr@gmail.com>
2021-10-16 15:19:06 -0400

    update useful_scripts

--- useful_scripts.sh
+++ useful_scripts.sh
@@ -1,5 +1,9 @@
 forall() {
 	COMMAND=$@
+	if [ ${#COMMAND[@]} -le 1 ]; then
+    	echo "No command entered! Please enter a command to run!"
+		return;
+	fi
 	echo "Running $COMMAND"
 	find . -maxdepth 1 \( ! -name . \) | grep -v \\./\\. | xargs -I {} bash -c "cd '{}' && echo && echo {} && $COMMAND";
 }
