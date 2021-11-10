b9bcf818d387bd6a063d6ae3865167cc3e06b48e
Robert Hilton <robert.a.hilton.jr@gmail.com>
2021-11-10 03:10:41 -0500

    Update spawn script

--- spawn_new_terminal.sh
+++ spawn_new_terminal.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 CONTAINER_ID=`docker ps -aq --filter "ancestor=guitar24t/ck-ros:latest" --filter "status=running"`
+
+if [ -z "${CONTAINER_ID}" ] 
+then
+    echo "No running ck-ros docker instance could be detected! You must first run the launch script!"
+    exit 1
+fi
+
 docker exec -it $CONTAINER_ID /bin/bash --rcfile /mnt/.bashrc
