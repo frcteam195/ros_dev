b2673a34197152d9fc18c5bea2c9f20a613f2b64
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-09-30 02:59:56 +0000

    Update

--- configurator.py
+++ configurator.py
@@ -6,4 +6,20 @@
 #Allow adding libraries and nodes to _node CMAKE files
 #Create new nodes?
 
-print("test")
+import os
+
+script_dir = os.path.abspath( os.path.dirname( __file__ ) )
+ 
+rootdir = os.path.normpath(script_dir + '/../')
+nodes = []
+robot_folder = ""
+for file in os.listdir(rootdir):
+    d = os.path.join(rootdir, file)
+    if os.path.isdir(d):
+        if d.endswith("_node"):
+            nodes.append(d)
+        if d.endswith("_Robot"):
+            robot_folder = d
+
+print(nodes)
+print(robot_folder)
