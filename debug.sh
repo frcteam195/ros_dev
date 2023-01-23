e928f2eecb4dedf35f4b2cef5e99e3d3c6f3e027
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-01-23 02:03:43 +0000

    Add ros source to debug.sh

--- debug.sh
+++ debug.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROBOT_FOLDER="$(cd ${SCRIPT_DIR}/../*_Robot && pwd)"
+source "${ROBOT_FOLDER}/catkin_ws/devel/setup.bash"
 export ROS_IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 export ROS_MASTER_URI=http://10.1.95.5:11311
